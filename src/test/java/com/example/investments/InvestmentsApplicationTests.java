0f5eaf61be98b191b2817b414f8af92659356bba
Denison Kolling <denisonkolling@gmail.com>
2024-01-06 08:39:39 -0300

    first commit

---
+++ src/test/java/com/example/investments/InvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
