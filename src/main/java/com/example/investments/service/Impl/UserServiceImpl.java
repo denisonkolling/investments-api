4b9db2066145ef1218b6f0a79536e04ba7a84910
Denison Kolling <denisonkolling@gmail.com>
2024-01-07 08:26:22 -0300

    feat: add user service CRUD methods

--- src/main/java/com/example/investments/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/investments/service/Impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.investments.service.Impl;
 
 import com.example.investments.model.User;
+import com.example.investments.repository.UserRepository;
 import com.example.investments.service.UserService;
 import org.springframework.stereotype.Service;
 
@@ -10,28 +11,63 @@ import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UUID createUser(User user) {
-        return null;
+    public User createUser(User user) {
+        return userRepository.save(user);
     }
 
     @Override
     public Optional<User> getUserById(String userId) {
-        return Optional.empty();
+        return userRepository.findById(UUID.fromString(userId));
     }
 
     @Override
     public List<User> listUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
-    public void updateUserById(String userId, User User) {
+    public void updateUserById(String userId, User user) {
+
+        var id = UUID.fromString(userId);
+
+        var userEntity = userRepository.findById(id);
+
+        if (userEntity.isPresent()) {
+
+            var userData = userEntity.get();
+
+            if (userData.getUsername() != null) {
+                user.setUsername(userData.getUsername());
+            }
+
+            if (user.getPassword() != null) {
+                user.setPassword(user.getPassword());
+            }
+
+            userRepository.save(userData);
+
+        }
 
     }
 
     @Override
     public void deleteById(String userId) {
+
+        var id = UUID.fromString(userId);
+
+        var userExists = userRepository.existsById(id);
+
+        if (userExists) {
+            userRepository.deleteById(id);
+        }
 
     }
 }
--- src/main/java/com/example/investments/service/UserService.java
+++ src/main/java/com/example/investments/service/UserService.java
@@ -4,11 +4,10 @@ import com.example.investments.model.User;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface UserService {
 
-    UUID createUser(User user);
+    User createUser(User user);
 
     Optional<User> getUserById(String userId);
 
